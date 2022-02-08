eb615c35175b320418c89711b5e61707dabade4a
GNIL <tony.vinz.one@gmail.com>
2022-02-09 00:55:39 +0300

    first commit

---
+++ slider.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', function(event){
+    $(document).ready(function(){
+        $('.slider').slick({
+            dots: false,
+            arrows: true,
+        });
+    });
+});
